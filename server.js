93a9b2617019d4f1bb6a5ff4424ae2627149cc29
Jianan Wang <jchen985@uwo.ca>
2023-11-09 15:36:36 -0500

    ./models/index.js exports User and Role collection schema; server.js creates initial 3 roles (User, Admin, Moderator) when no Roles exits

--- server.js
+++ server.js
@@ -22,6 +22,61 @@ app.use(
     })
 );
 
+// open Mongoose connection to MongoDB database
+const db = require("./models/index");  // db is an object that constains the Role and User collection 
+const dbConfig = require("./config/db.config");
+const Role = db.role;
+
+db.mongoose.connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`).then(() => {
+    console.log("Successfully connected to MongoDB");
+    initial();
+}).catch(err => {
+    console.error("Connection error: ", err);
+    process.exit();
+});
+
+
+/* 
+create 3 rows in roles collection
+"user", "moderator", "admin"
+*/
+async function initial() {
+
+    await Role.estimatedDocumentCount().then((count) => {
+
+        console.log(count);
+        if (count === 0){
+            new Role({
+                name: "user"
+            }).save().then(() => {
+                console.log("added 'user' to Role collection");
+            }).catch(err => {
+                console.error("error", err);
+            });
+
+            new Role({
+                name: "moderator"
+            }).save().then(() => {
+                console.log("added 'moderator' to Role collection");
+            }).catch(err => {
+                console.error("error", err);
+            });
+
+            new Role({
+                name: "admin"
+            }).save().then(() => {
+                console.log("added 'admin' to Role collection");
+            }).catch(err => {
+                console.error("error", err);
+            });
+        }
+
+    }).catch(err => {
+        console.error("Estimate Doc Count error: ", err);
+        // process.exit();
+    });
+
+}
 
 //simple route
 app.get("/", (req, res) => {
